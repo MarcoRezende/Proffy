689bdba17f3993bf0f3a9e5b0f5e8a0eabb471d6
Marco Rezende <marcorezende@outlook.com.br>
2020-09-03 22:35:45 -0300

    update

--- web/src/pages/SignUp/index.tsx
+++ web/src/pages/SignUp/index.tsx
@@ -1,5 +1,7 @@
 import React, { useState, FormEvent } from 'react';
 
+import backIcon from '../../assets/images/icons/back.svg';
+import logoIcon from '../../assets/images/logo.svg';
 import { ReactComponent as EyeVisibleIcon } from '../../assets/images/icons/visibility.svg';
 import { ReactComponent as EyeInvisibleIcon } from '../../assets/images/icons/invisible.svg';
 
@@ -17,34 +19,39 @@ function TeacherList() {
   return (
     <div id="sign-up-page" className="container">
       <main className="sign-up-inner">
-        <form className="sign-up-form">
-          <div className="fieldset">
-            <legend>
-              <p>Cadastro</p>
-              <p>Preencha os dados abaixo para começar.</p>
-            </legend>
+        <div className="wrapper">
+          <a href="/"><img src={backIcon} /></a>
+          <form className="sign-up-form">
+            <div className="fieldset">
+              <legend>
+                <p>Cadastro</p>
+                <p>Preencha os dados abaixo para <br/> começar.</p>
+              </legend>
 
-            <div className="sign-up-input-block">
-              <input type="text" placeholder="Nome" name="name" />
-              <input type="text" placeholder="Sobrenome" name="last-name" />
-              <input type="email" placeholder="E-mail" name="email" />
-              <span className="password-input">
-                <input
-                  type={ togglePasswordVisibility ? 'text' : 'password' }
-                  placeholder="Senha"
-                  name="password"
-                />
-                <span className="input-icon" onClick={handlerTogglePasswordVisibility}>
-                  { togglePasswordVisibility ? <EyeInvisibleIcon/> : <EyeVisibleIcon/> }
+              <div className="sign-up-input-block">
+                <input type="text" placeholder="Nome" name="name" />
+                <input type="text" placeholder="Sobrenome" name="last-name" />
+                <input type="email" placeholder="E-mail" name="email" />
+                <span className="password-input">
+                  <input
+                    type={ togglePasswordVisibility ? 'text' : 'password' }
+                    placeholder="Senha"
+                    name="password"
+                  />
+                  <span className="input-icon" onClick={handlerTogglePasswordVisibility}>
+                    { togglePasswordVisibility ? <EyeInvisibleIcon/> : <EyeVisibleIcon/> }
+                  </span>
                 </span>
-              </span>
+              </div>
             </div>
-          </div>
-          <button type="submit">Concluir cadastro</button>
-        </form>
+            <button type="submit">Concluir cadastro</button>
+          </form>
+        </div>
         <div className="sign-up-hero">
-          <span></span>
-          <div></div>
+          <div>
+            <img src={logoIcon} />
+            <p>Sua plataforma de <br /> estudos online.</p>
+          </div>
         </div>
       </main>
     </div>
